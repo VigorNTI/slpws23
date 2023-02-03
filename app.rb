b09f2ba1b9ba8f6e07c5e34cc290ee82e328316a
Snakehater <vigor.turujlija@gmail.com>
2023-02-03 14:04:26 +0100

    Added C for orders and C for order_products, TODO: RU, delete will not be used as of now

--- app.rb
+++ app.rb
@@ -109,6 +109,30 @@ def check_admin()
     halt 401, {'Content-Type' => 'text/plain'}, 'You should not be here'
   end
 end
+###################################################################################################################################
+#########################################################   CHECKOUT   ############################################################
+###################################################################################################################################
+
+before("/checkout") do
+  if !get_user() then
+    redirect('/')
+  end
+end
+
+post("/checkout") do
+  uid = session[:id].to_i
+  db = grab_db()
+  shoppingcart = db.execute("SELECT * FROM shoppingcart WHERE user_id=?", uid)
+  if shoppingcart.length > 0 then
+    db.execute("INSERT INTO orders (user_id,date) VALUES (?,?)", uid, Time.now.to_i * 1000)
+    order_id = db.last_insert_row_id
+    shoppingcart.each do |item|
+      db.execute("INSERT INTO order_products (order_id, product_id, amount) VALUES (?,?,?)", order_id, item["product_id"], item["amount"]);
+      db.execute("DELETE FROM shoppingcart WHERE user_id=? AND product_id=?", uid, item["product_id"])
+    end
+  end
+  redirect('/products')
+end
 
 ###################################################################################################################################
 #######################################################   SHOPPINGCART   ##########################################################
